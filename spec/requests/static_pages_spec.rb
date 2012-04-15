deb3fccb5e415c2b5e130f7f8a4da9f487d06b4d
Steve Sneddon <sned@stevesneddon.us>
2012-04-15 13:49:03 -0700

    Checkpoint static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the content 'LoboBlanca'" do
+      visit '/static_pages/home'
+      page.should have_content('LoboBlanca')
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+end
