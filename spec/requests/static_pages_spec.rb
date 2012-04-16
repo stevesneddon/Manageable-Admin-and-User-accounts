2e78b6decd6ec2fb07c3f0998f192dcdb1c4a066
Steve Sneddon <sned@stevesneddon.us>
2012-04-16 14:09:25 -0700

    Basic CSS done

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,65 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'LoboBlanca'" do
-      visit '/static_pages/home'
-      page.should have_content('LoboBlanca')
+
+    it "should have h1 'Dedication'" do
+      visit root_path
+      page.should have_selector('span', text: 'Dedication')
+    end
+
+    it "should have the base title" do
+      visit root_path
+      page.should have_selector('title',
+                        text: "LoboBlanca")
+    end
+
+    it "should not have a custom page title" do
+      visit root_path
+      page.should_not have_selector('title', text: '| Home')
     end
   end
+
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
+
+    it "should have the h1 'Help'" do
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit help_path
+      page.should have_selector('title',
+                        text: "Help")
     end
   end
+
   describe "About page" do
-    it "should have the content 'About'" do
-      visit '/static_pages/about'
-      page.should have_content('About')
+
+    it "should have the h1 'About'" do
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit about_path
+      page.should have_selector('title',
+                    text: "About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title',
+                    text: "Contact")
     end
   end
 end
