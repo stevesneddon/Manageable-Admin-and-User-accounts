9560fd3b585c1e6efd3bd110e2d52389702649f7
Steve Sneddon <sned@stevesneddon.us>
2012-04-25 17:54:41 -0700

    Finish user/admin signup

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,5 +4,15 @@ class AdminsController < ApplicationController
   end
 
   def new
+    @admin = Admin.new
+  end
+  def create
+    @admin = Admin.new(params[:admin])
+    if @admin.save
+      flash[:success] = "Welcome to LoboBlanca.org!"
+      redirect_to @admin
+    else
+      render 'new'
+    end
   end
 end
--- spec/requests/admin_pages_spec.rb
+++ spec/requests/admin_pages_spec.rb
@@ -18,4 +18,29 @@ describe "Admin pages" do
     it { should have_selector('h1',    text: 'Admin Sign up') }
     it { should have_selector('title', text: full_title('Admin')) }
   end
+  describe "new ops person" do
+
+    before { visit newops_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(Admin, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example Admin"
+        fill_in "Email",        with: "admin@example.com"
+        fill_in "Password",     with: "foobarbaz"
+        fill_in "Confirmation", with: "foobarbaz"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(Admin, :count).by(1)
+      end
+    end
+  end
 end
